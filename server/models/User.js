18828596206ecb23e631dc92a1735917fe677189
roomiesofficial522-svg <roomiesofficial522@gmail.com>
2026-01-22 22:39:27 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,3 @@
-// server/models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -27,5 +26,6 @@ const userSchema = new mongoose.Schema({
     default: Date.now 
   }
 });
+
 
 module.exports = mongoose.model('User', userSchema);
